b54481f2812dee32ac3e88809f000fa8403e1c34
Jefferson daniel(Work) <jeffersondaniels@hotmail.com>
2014-08-19 16:49:35 -0300

    Modularizando a estrutura de arquivos Front-end

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?> <!-- Inclue código do header-->
+
+<body>
+
+<?php include 'sidebar.php'; ?> <!-- Inclue código da sidebar-->
+</body>
+
+<?php include 'footer.php'; ?> <!-- Inclue código do Footer-->
